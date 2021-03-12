a72eda427b5cc8492fcf82551cd7168d6168ea73
Msii <geroi_cool@mail.ru>
2021-03-12 20:38:10 +0300

    5 заdание выполнено

--- lesson4/task5.py
+++ lesson4/task5.py
@@ -13,6 +13,7 @@ import requests
 import decimal
 import sys
 
+
 def get_currency_rate(valute):
     site = requests.get('http://www.cbr.ru/scripts/XML_daily.asp')
     text = site.content.decode('windows-1251')
@@ -23,7 +24,6 @@ def get_currency_rate(valute):
             break
     else:
         return print('None')
-
     one_part_text, second_part_text = need_str.split('<Value>')
     one_part_valute, second_part_valute = second_part_text.split('</Value>')
     one_part_text_day, second_part_text_day = text.split('Date="')
