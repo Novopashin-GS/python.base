5c446114f489811ee9ad298cd511f220305fbb32
Msii <geroi_cool@mail.ru>
2021-03-28 23:25:17 +0300

    1 заdание выполнено

--- lesson6/task1.py
+++ lesson6/task1.py
@@ -1,3 +1,22 @@
-f = open('nginx_logs.txt', 'r', encoding='utf-8')
-for line in f:
-    print(line)
+# 1. Не используя библиотеки для парсинга, распарсить (получить определённые данные) файл логов web-сервера
+# nginx_logs.txt (https://github.com/elastic/examples/raw/master/Common%20Data%20Formats/nginx_logs/nginx_logs)
+# — получить список  кортежей вида: (<remote_addr>, <request_type>, <requested_resource>). Например:
+# [
+# ...
+# ('141.138.90.60', 'GET', '/downloads/product_2'),
+# ('141.138.90.60', 'GET', '/downloads/product_2'),
+# ('173.255.199.22', 'GET', '/downloads/product_2'),
+# ...
+# ]
+# Примечание: код должен работать даже с файлами, размер которых превышает объем ОЗУ компьютера.
+
+
+with open('nginx_logs.txt', 'r', encoding='utf8') as f:
+    need_list = []
+    for line in f:
+        remote_addr = line.split(' - - ')[0]
+        request_type = line.split('/downloads')[0].split('"')[1]
+        requested_resource = '/' + line.split(' HTTP')[0].split(' /')[1]
+        need_tuple = (remote_addr, request_type, requested_resource)
+        need_list.append(need_tuple)
+    print(need_list)
